76cb9a791dcca41a15b8a49a44992c935e5999cb
Prohor <DKoval777@yandex.ru>
2023-10-16 20:17:34 +0300

    DZ_parsing_func.py

--- DZ_parsing_func.py
+++ DZ_parsing_func.py
@@ -3,23 +3,6 @@ from bs4 import BeautifulSoup
 import csv
 
 
-# parse_data = []
-# url = "https://www.ixbt.com/live/index/news/"
-# r = requests.get(url).text
-# soup = BeautifulSoup(r, "lxml")
-# data = soup.find_all("div", class_="thumbnail")
-# for element in data:
-#     header = element.find("h3", class_="topic-title").text
-#     time = element.find("li", class_="topic-info-date").text.strip()
-#     category = element.find("li", class_="topic-info-blog").text
-#     parse_data.append({"time": time, "category": category, "header": header})
-#
-# with open("parse_news.csv", "w") as f:
-#     for i in parse_data:
-#
-#         f.write(f"{i['time']}: {i['category']} - {i['header']}\n")
-
-
 def get_html(url):
     response = requests.get(url)
     return response.text
